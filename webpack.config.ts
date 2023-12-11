081cc56b7e88e8e22dc4db640c618645a9962c6e
arkaris <myachkovd@mail.ru>
2023-12-11 17:40:24 +0400

    webpack config with ts

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from "path"
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from "webpack"
+
+type Mode = "production" | "development";
+
+interface Env {
+	mode: Mode
+}
+
+export default (env: Env) => {
+	const config: webpack.Configuration = {
+		mode: env.mode ?? "development",
+		entry: path.resolve(__dirname, 'src', 'index.ts'),
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: '[name].[contenthash].js',
+			clean: true
+		},
+		plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })],
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+	}
+	return config
+}
