54be162fbb03bf40a22bbb75748ba9aa5bb3a8f8
arkaris <myachkovd@mail.ru>
2023-12-11 14:59:13 +0400

    typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = (env) => ({
 	mode: env.mode ?? "development",
-	entry: path.resolve(__dirname, 'src', 'index.js'),
+	entry: path.resolve(__dirname, 'src', 'index.ts'),
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: '[name].[contenthash].js',
 		clean: true
 	},
-	plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })]
+	plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
 })
