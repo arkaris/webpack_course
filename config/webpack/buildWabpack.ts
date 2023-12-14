b0e841ca9f779b9077c07ee0cf9be8bb90300d27
arkaris <myachkovd@mail.ru>
2023-12-14 17:08:51 +0400

    update source-map

--- config/webpack/buildWabpack.ts
+++ config/webpack/buildWabpack.ts
@@ -22,7 +22,7 @@ export function buildWebpack(options: WebpackOptions): Configuration {
 			rules: buildLoaders(options),
 		},
 		resolve: buildResolvers(options),
-		devtool: isDev && "inline-source-map",
+		devtool: isDev && "eval-cheap-module-source-map",
 		devServer: isDev ? buildDevServer(options) : undefined
 	}
 	return config
